3c00eb9b5fcacc868390e70fb0a77f896a846225
Mikhail <mishail.lukushevich@gmail.com>
2025-03-19 22:25:11 +0300

    Animal and Util classes
    - added Animal class;
    - added Util class with custom comparing and validate methods;

---
+++ src/main/java/org/CustomClass/Animal.java
@@ -0,0 +1,116 @@
+package org.CustomClass;
+
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class Animal implements Externalizable, Comparable<Animal> {
+    private static final Long serialVersionUID = 1L;
+    private String species;
+    private String eyeColor;
+    private boolean isWool;
+    private int age;
+
+    private Animal(Builder builder) {
+        this.species = builder.species;
+        this.eyeColor = builder.eyeColor;
+        this.isWool = builder.isWool;
+        this.age = builder.age;
+    }
+
+    public Animal(){}
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public String getEyeColor() {
+        return eyeColor;
+    }
+
+    public boolean isWool() {
+        return isWool;
+    }
+
+    @Override
+    public int compareTo(Animal o) {
+        int comparison = Util.comparingString(Animal::getSpecies).compare(this, o);
+        if (comparison != 0) return comparison;
+
+        comparison = Util.comparingString(Animal::getEyeColor).compare(this, o);
+        if (comparison != 0) return comparison;
+
+        return Boolean.compare(this.isWool, o.isWool);
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "species='" + species + '\'' +
+                ", eyeColor='" + eyeColor + '\'' +
+                ", isWool=" + isWool + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(species);
+        out.writeObject(eyeColor);
+        out.writeBoolean(isWool);
+        out.writeInt(age);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        String tSpecies = (String) in.readObject();
+        if (Util.isStringValid(tSpecies)) species = tSpecies;
+        else throw new ClassCastException("Invalid species");
+
+        String tEyeColor = (String) in.readObject();
+        if (Util.isStringValid(tEyeColor)) eyeColor = tEyeColor;
+        else throw new ClassCastException("Invalid eyeColor");
+
+        isWool = in.readBoolean();
+        int tAge = in.readInt();
+        if (Util.isIntValid(tAge)) age = tAge;
+        else throw new ClassCastException("Invalid age");
+    }
+
+    public static class Builder {
+        private String species;
+        private String eyeColor;
+        private boolean isWool;
+        private int age;
+
+        public Builder() {}
+
+        public Builder species(String species) throws Exception {
+            if (!Util.isStringValid(species)) throw new Exception("Invalid species");
+            this.species = species;
+            return this;
+        }
+
+        public Builder eyeColor(String eyeColor) throws Exception {
+            if (!Util.isStringValid(eyeColor)) throw new Exception("Invalid eyeColor");
+            this.eyeColor = eyeColor;
+            return this;
+        }
+
+        public Builder wool(boolean wool) {
+            isWool = wool;
+            return this;
+        }
+
+        public Builder age(int age) throws Exception {
+            if (!Util.isIntValid(age)) throw new Exception("Invalid age");
+            this.age = age;
+            return this;
+        }
+
+        public Animal build() {
+            return new Animal(this);
+        }
+    }
+}
---
+++ src/main/java/org/CustomClass/Util.java
@@ -0,0 +1,64 @@
+package org.CustomClass;
+
+import java.util.Comparator;
+import java.util.function.Function;
+
+public final class Util {
+    public static <T> Comparator<T> comparingInt(Function<T, Integer> keyExtractor) {
+        return (o1, o2) -> {
+            int key1 = keyExtractor.apply(o1);
+            int key2 = keyExtractor.apply(o2);
+            return key1 - key2;
+        };
+    }
+
+    public static <T> Comparator<T> comparingBoolean(Function<T, Boolean> keyExtractor) {
+        return (o1, o2) -> {
+            boolean key1 = keyExtractor.apply(o1);
+            boolean key2 = keyExtractor.apply(o2);
+
+            if (key1 == key2) {
+                return 0;
+            } else if (key1 && !key2) {
+                return 1;
+            } else {
+                return -1;
+            }
+        };
+    }
+
+    public static <T> Comparator<T> comparingString(Function<T, String> keyExtractor) {
+        return (o1, o2) -> {
+            String key1 = keyExtractor.apply(o1);
+            String key2 = keyExtractor.apply(o2);
+            int minLength = Math.min(key1.length(), key2.length());
+
+            for (int i = 0; i < minLength; i++) {
+                char c1 = key1.charAt(i);
+                char c2 = key2.charAt(i);
+
+                if (c1 != c2)
+                    return c1 - c2;
+            }
+
+            return key1.length() - key2.length();
+        };
+    }
+
+    public static boolean isIntValid(int value) {
+        return value > 0 && value <= 150;
+    }
+
+    public static boolean isStringValid(String value) {
+        if (value.isBlank() || value.isEmpty()) return false;
+        if (value.length() < 3 || value.length() > 20) return false;
+
+        for (char c : value.toCharArray()) {
+            if (Character.isDigit(c)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
